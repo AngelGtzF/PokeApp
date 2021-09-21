815f32e632e8bae0a7e56cafce637d00244a8d6a
AngelGTZF <angelgf10@hotmail.es>
2021-09-21 14:10:31 -0500

    version estable del proyecto

---
+++ src/main/java/com/codeInter/pokeApi/PokeApiCodeInt/PokeApiCodeIntApplication.java
@@ -0,0 +1,13 @@
+package com.codeInter.pokeApi.PokeApiCodeInt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApiCodeIntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApiCodeIntApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeInter/pokeApi/PokeApiCodeInt/PokeApiCodeIntApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeInter.pokeApi.PokeApiCodeInt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApiCodeIntApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
